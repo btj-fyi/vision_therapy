207772b0df60d2134186be0ba08b4ed1d8f12ba2
btj-fyi <me@btj.fyi>
2025-02-07 02:55:36 -0800

    Can't be asked.

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 from flask import Flask, send_file
 import os
 from peripheral_awareness.peripheral_awareness import main
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 
 @app.route("/")
@@ -12,3 +14,7 @@ def myapp():
     return send_file(
         "peripheral_awareness\peripheral_awareness.png", mimetype="image/png"
     )
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
