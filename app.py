09aa2220215fdff656252ae1f154c0afc00109be
btj-fyi <me@btj.fyi>
2025-02-07 04:24:37 -0800

    Can't be asked.

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 from flask import Flask, send_file
 import os
 from peripheral_awareness.peripheral_awareness import main
-from werkzeug.middleware.proxy_fix import ProxyFix
 
 app = Flask(__name__)
-app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_prefix=1)
 
 
 @app.route("/")
